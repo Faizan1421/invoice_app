7afcf625d04d691f3198d7076471b9c4d2e032e0
faizan <faizantayyab37@gmail.com>
2024-12-23 18:48:47 +0500

    updated ui

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -10,22 +10,46 @@ import {
 } from "@/components/ui/table"
 
 import { Badge } from "@/components/ui/badge"
+import { Button } from "@/components/ui/button";
+import { CirclePlus } from "lucide-react";
+import Link from "next/link";
 
 
 export default function Home() {
     return (
-        <main className="flex flex-col gap-6 items-center justify-center h-[100vh] text-center max-w-5xl mx-auto ">
+        <main className="flex flex-col gap-6  h-[100vh] text-center max-w-5xl mx-auto my-12">
 
-            <h1 className="text-5xl font-bold">Dashboard</h1>
+            <div className="flex  justify-between">
+                <h1 className="text-3xl font-semibold">Invoices</h1>
+                <p>
+                    <Button variant="ghost" className="inline-flex gap-2" asChild>
+                        <Link href="/invoices/new">
+                            <CirclePlus className="h-4 w-4 " />
+                            Create Invoice
+                        </Link>
+                    </Button>
+                </p>
+            </div>
+
             <Table>
                 <TableCaption>A list of your recent invoices.</TableCaption>
                 <TableHeader>
                     <TableRow>
-                        <TableHead className="w-[100px]">Date</TableHead>
-                        <TableHead>Customer</TableHead>
-                        <TableHead>Email</TableHead>
-                        <TableHead>Status</TableHead>
-                        <TableHead className="text-right">Value</TableHead>
+                        <TableHead className="w-[100px] p-4">
+                            Date
+                        </TableHead>
+                        <TableHead className="p-4">
+                            Customer
+                        </TableHead>
+                        <TableHead className="p-4">
+                            Email
+                        </TableHead>
+                        <TableHead>
+                            Status
+                        </TableHead>
+                        <TableHead className="text-right p-4">
+                            Value
+                        </TableHead>
                     </TableRow>
                 </TableHeader>
                 <TableBody>
@@ -34,14 +58,13 @@ export default function Home() {
                             <span className="font-semibold">23/12/2024</span>
                         </TableCell>
                         <TableCell className="text-left">
+                            <span className="font-semibold">Faizan Tayyab</span>
+                        </TableCell>
+                        <TableCell className="text-left">
                             <span className="">Faizan1421@gmail.com</span>
                         </TableCell>
                         <TableCell className="text-left">
-                        <Badge className="rounded-full">Open</Badge>
-                            
-                        </TableCell>
-                        <TableCell className="text-left">
-                            <span className="font-semibold">Faizan Tayyab</span>
+                            <Badge className="rounded-full">Open</Badge>
                         </TableCell>
                         <TableCell className="text-right">
                             <span className="font-semibold">$250.00</span>
