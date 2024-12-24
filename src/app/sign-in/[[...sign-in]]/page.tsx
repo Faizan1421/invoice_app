3d45e39c87aacc319dc17f6952723e335bc47268
faizan <faizantayyab37@gmail.com>
2024-12-24 18:51:59 +0500

    updated ui of sign in

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -5,7 +5,7 @@ import { Button } from '@/components/ui/button'
 import * as Clerk from '@clerk/elements/common'
 import * as SignIn from '@clerk/elements/sign-in'
 
-import {AnimatePresence, motion} from 'framer-motion'
+import { AnimatePresence, motion } from 'framer-motion'
 
 export default function SignInPage() {
   return (
@@ -65,27 +65,27 @@ export default function SignInPage() {
                   name="google"
                   asChild
                 >
-                  <Button className='w-full font-bold'> 
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 16 16"
-                    aria-hidden
-                    className="size-4"
-                  >
-                    <g clipPath="url(#a)">
-                      <path
-                        fill="currentColor"
-                        d="M8.32 7.28v2.187h5.227c-.16 1.226-.57 2.124-1.192 2.755-.764.765-1.955 1.6-4.035 1.6-3.218 0-5.733-2.595-5.733-5.813 0-3.218 2.515-5.814 5.733-5.814 1.733 0 3.005.685 3.938 1.565l1.538-1.538C12.498.96 10.756 0 8.32 0 3.91 0 .205 3.591.205 8s3.706 8 8.115 8c2.382 0 4.178-.782 5.582-2.24 1.44-1.44 1.893-3.475 1.893-5.111 0-.507-.035-.978-.115-1.369H8.32Z"
-                      />
-                    </g>
-                    <defs>
-                      <clipPath id="a">
-                        <path fill="#fff" d="M0 0h16v16H0z" />
-                      </clipPath>
-                    </defs>
-                  </svg>
-                  Login with Google
+                  <Button className='w-full font-bold'>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 16 16"
+                      aria-hidden
+                      className="size-4"
+                    >
+                      <g clipPath="url(#a)">
+                        <path
+                          fill="currentColor"
+                          d="M8.32 7.28v2.187h5.227c-.16 1.226-.57 2.124-1.192 2.755-.764.765-1.955 1.6-4.035 1.6-3.218 0-5.733-2.595-5.733-5.813 0-3.218 2.515-5.814 5.733-5.814 1.733 0 3.005.685 3.938 1.565l1.538-1.538C12.498.96 10.756 0 8.32 0 3.91 0 .205 3.591.205 8s3.706 8 8.115 8c2.382 0 4.178-.782 5.582-2.24 1.44-1.44 1.893-3.475 1.893-5.111 0-.507-.035-.978-.115-1.369H8.32Z"
+                        />
+                      </g>
+                      <defs>
+                        <clipPath id="a">
+                          <path fill="#fff" d="M0 0h16v16H0z" />
+                        </clipPath>
+                      </defs>
+                    </svg>
+                    Login with Google
                   </Button>
                 </Clerk.Connection>
               </div>
@@ -144,21 +144,21 @@ export default function SignInPage() {
             </SignIn.Strategy>
             <SignIn.Strategy name="totp">
               <header className="text-center">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                fill="none"
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                className="mx-auto size-10 text-zinc-950 dark:text-white"
-                viewBox="0 0 24 24"
-              >
-                <path d="M16 20V4a2 2 0 0 0-2-2h-4a2 2 0 0 0-2 2v16"></path>
-                <rect width="20" height="14" x="2" y="6" rx="2"></rect>
-              </svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  className="mx-auto size-10 text-zinc-950 dark:text-white"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M16 20V4a2 2 0 0 0-2-2h-4a2 2 0 0 0-2 2v16"></path>
+                  <rect width="20" height="14" x="2" y="6" rx="2"></rect>
+                </svg>
                 <h1 className="mt-4 text-xl font-medium tracking-tight text-neutral-950">
                   Verify Authenticator Code
                 </h1>
@@ -167,44 +167,46 @@ export default function SignInPage() {
               <Clerk.Field name="code">
                 <Clerk.Label className="sr-only">Authenticator Code</Clerk.Label>
                 <Clerk.Input
-  type="otp"
-  required
-  className="flex justify-center gap-1"
-  render={({ value, status }) => (
-    <div
-      data-status={status}
-      className="relative h-9 w-8 rounded-md bg-white ring-1 ring-inset ring-zinc-300 data-[status=selected]:bg-sky-400/10 data-[status=selected]:shadow-[0_0_8px_2px_theme(colors.sky.400/30%)] data-[status=selected]:ring-sky-400"
-    >
-      <AnimatePresence>
-        {value && (
-          <motion.span
-            initial={{ opacity: 0, scale: 0.75 }}
-            animate={{ opacity: 1, scale: 1 }}
-            exit={{ opacity: 0, scale: 0.75 }}
-            className="absolute inset-0 flex items-center justify-center text-zinc-950"
-          >
-            {value}
-          </motion.span>
-        )}
-        {value}
-      </AnimatePresence>
-      {status === 'cursor' && (
-        <motion.div
-          layoutId="otp-input-focus"
-          transition={{ ease: [0.2, 0.4, 0, 1], duration: 0.2 }}
-          className="absolute inset-0 z-10 rounded-[inherit] border border-sky-400 bg-sky-400/10 shadow-[0_0_8px_2px_theme(colors.sky.400/30%)]"
-        />
-      )}
-    </div>
-  )}
-/>
+                  type="otp"
+                  required
+                  className="flex justify-center gap-1"
+                  render={({ value, status }) => (
+                    <div
+                      data-status={status}
+                      className="relative h-9 w-8 rounded-md bg-white ring-1 ring-inset ring-zinc-300 data-[status=selected]:bg-sky-400/10 data-[status=selected]:shadow-[0_0_8px_2px_theme(colors.sky.400/30%)] data-[status=selected]:ring-sky-400"
+                    >
+                      <AnimatePresence>
+                        {value && (
+                          <motion.span
+                            initial={{ opacity: 0, scale: 0.75 }}
+                            animate={{ opacity: 1, scale: 1 }}
+                            exit={{ opacity: 0, scale: 0.75 }}
+                            className="absolute inset-0 flex items-center justify-center text-zinc-950"
+                          >
+                            {value}
+                          </motion.span>
+                        )}
+                        {value}
+                      </AnimatePresence>
+                      {status === 'cursor' && (
+                        <motion.div
+                          layoutId="otp-input-focus"
+                          transition={{ ease: [0.2, 0.4, 0, 1], duration: 0.2 }}
+                          className="absolute inset-0 z-10 rounded-[inherit] border border-sky-400 bg-sky-400/10 shadow-[0_0_8px_2px_theme(colors.sky.400/30%)]"
+                        />
+                      )}
+                    </div>
+                  )}
+                />
                 <Clerk.FieldError className="mt-2 block text-xs text-red-600" />
               </Clerk.Field>
               <SignIn.Action
                 submit
-                className="relative w-full rounded-md bg-neutral-600 bg-gradient-to-b from-neutral-500 to-neutral-600 py-1.5 text-sm text-white shadow-[0_1px_1px_0_theme(colors.white/10%)_inset,0_1px_2.5px_0_theme(colors.black/36%)] outline-none ring-1 ring-inset ring-neutral-600 before:absolute before:inset-0 before:rounded-md before:bg-white/10 before:opacity-0 hover:before:opacity-100 focus-visible:outline-offset-2 focus-visible:outline-neutral-600 active:bg-neutral-600 active:text-white/60 active:before:opacity-0"
-              >
-                Login
+                asChild              >
+                <Button className='w-full font-bold'>
+
+                  Log In
+                </Button>
               </SignIn.Action>
             </SignIn.Strategy>
             <p className="text-center text-sm text-neutral-500">
